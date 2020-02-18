2360e5a8de19544be82ca226e06c1b8ef1f2c56f
Claarice <claricepereira.ribeiro@gmail.com>
2020-02-17 23:40:59 -0300

    Chore: update dashboard

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import logo from '~/assets/logo-purple.svg';
@@ -8,6 +9,8 @@ import Notifications from '~/components/Notifications';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -20,12 +23,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Clarice Pereira</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Clarice"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
